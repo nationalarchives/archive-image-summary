e39d50dc1aa3450b133d6cf65382709b86acb366
techncl <nigel_lutale@hotmail.co.uk>
2020-01-07 16:24:56 +0000

    added ability to run via commandline

--- src/main/scala/CountCPTifs.scala
+++ src/main/scala/CountCPTifs.scala
@@ -6,62 +6,66 @@ import com.github.tototoshi.csv._
 
 object CountCPTifs extends App {
 
-  def isTxtFile(file: String): Unit = {
+  def isATxtFile(file: String): Unit = {
     require(file.takeRight(4) == ".txt", s"Expected a .txt file but received '$file'. Please fix and try again!")
   }
 
   def getFileLines(txtFileName: String, txtFile:BufferedSource): Seq[String] = {
-    val linesOfFile: Seq[String] = txtFile.getLines().toSeq
+    val linesOfFile: Seq[String] = txtFile.getLines().toSeq //
     require(linesOfFile != Seq(), s"The content of the file '$txtFileName' is empty. Please fix and try again!")
     linesOfFile
   }
 
   def findRelevantColumnsForCsv(fileLine: String):(String, String, String)  = {
-    val relevantFileInfoPattern: Regex = "ENGLTNA1D\\_([a-zA-Z]+)(\\d+)\\-Box(\\d+).*$".r
+    val relevantFileInfoPattern: Regex = "ENGLTNA1D\\_([a-zA-Z]+)(\\d+)\\-Box(\\d+).*$".r // find record details
     val relevantFileInfoMatches: Iterator[Regex.Match] = relevantFileInfoPattern.findAllIn(fileLine).matchData
     val seqOfGroupsOfRecordDetails: Seq[(String, String, String)] = relevantFileInfoMatches.map {
-      recordDetails => (recordDetails.group(1),
-                        recordDetails.group(2),
-                        recordDetails.group(3))
+      recordDetails => (recordDetails.group(1), // department
+                        recordDetails.group(2), // series
+                        recordDetails.group(3)) // piece
     }.toSeq
-    seqOfGroupsOfRecordDetails.head
+    seqOfGroupsOfRecordDetails.head // take tuple out of the Seq
   }
 
   def findRelevantTifs(fileLines: Seq[String]): Seq[(String, String, String)] = {
     fileLines.collect{
-      case fileLine if(fileLine.takeRight(4) == ".tif") => findRelevantColumnsForCsv(fileLine)
+      case fileLine if(fileLine.takeRight(4) == ".tif") => findRelevantColumnsForCsv(fileLine) // get details of only tifs
     }
   }
 
   def generateTally(infoPerTifFile: Seq[(String, String, String)]): Map[(String, String, String), Int] = {
-    val tifTally: mutable.Map[(String, String, String), Int] = mutable.Map()
-//    val tifTally = infoPerTifFile.groupBy(info => info).view.mapValues {
-//      recordList => recordList.size
-//    }.toMap
-    infoPerTifFile.foreach { tifInfo =>
-      try {
-        tifTally(tifInfo) += 1
-      }
-      catch {
-        case _: NoSuchElementException => tifTally += tifInfo -> 1
-      }
-    }
-    tifTally.toMap
+//    val tifTally: mutable.Map[(String, String, String), Int] = mutable.Map()
+    val tifTally = infoPerTifFile.groupBy(recordDetail => recordDetail).view.mapValues { // group duplicate details
+      recordList => recordList.size // count number of record details duplicates
+    }.toMap
+
+//    infoPerTifFile.foreach { tifInfo =>
+//      try {
+//        tifTally(tifInfo) += 1
+//      }
+//      catch {
+//        case _: NoSuchElementException => tifTally += tifInfo -> 1
+//      }
+//    }
+    tifTally
   }
 
   def generateFinalCsv(csvFileName: String, tifTally: Map[(String, String, String), Int]) = {
-    val writer = CSVWriter.open("test"+csvFileName)
+    val writer = CSVWriter.open(csvFileName)
     writer.writeRow(List("Dept", "Series", "RefPiece", "ImageFormat", "NumberOfImages"))
 
-    for(((dept, series, pieceNumber), tally) <- tifTally){
-      writer.writeRow(List(dept, series, pieceNumber, "TIFF", tally))
+    for(((dept, series, pieceNumber), numOfTifs) <- tifTally){
+      writer.writeRow(List(dept, series, pieceNumber, "TIFF", numOfTifs))
     }
     writer.close()
 
   }
 
-  val txtFileName = "/home/nigel/Documents/Projects/Chelsea Pensioners/CP_062.txt"
-  isTxtFile(txtFileName)
+  println(args(0))
+  println("test")
+
+  val txtFileName = args(0)
+  isATxtFile(txtFileName)
 
   val txtFile: BufferedSource = Source.fromFile(txtFileName)
   val fileLines: Seq[String] = getFileLines(txtFileName, txtFile)
@@ -69,7 +73,7 @@ object CountCPTifs extends App {
 
   val tifTally: Map[(String, String, String), Int] = generateTally(recordDetailsPerTifFile)
 
-  val csvFileName: String = args(0).replace(".txt",".csv")
+  val csvFileName: String = txtFileName.replace(".txt",".csv")
   generateFinalCsv(csvFileName, tifTally)
 
 }
